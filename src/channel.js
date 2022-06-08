3651814487c53aec12aa718ece5b6e3eaa8218a4
Anson Lai <z5361987@ad.unsw.edu.au>
2022-06-08 22:09:47 +1000

    added stub functions channelDetailsV1 & channelJoinV1

--- src/channel.js
+++ src/channel.js
@@ -16,3 +16,17 @@ function channelInviteV1(authUserId, channelId, uId) {
 function channelMessagesV1(authUserId, channelId, start) {
     return 'authUserId' + 'channelId' + 'start';
 }
+
+// Sample stub for a function 'channelDetailsV1', 
+// with arguments named 'authUserId', 'channelId'
+// Returns a string concatenating 'authUserId', 'channelId'
+function channelDetailsV1(authUserId, channelId) {
+    return 'authUserId' + 'channelId';
+}
+
+// Sample stub for a function 'channelDetailsV1', 
+// with arguments named 'authUserId', 'channelId'
+// Returns a string concatenating 'authUserId', 'channelId'
+function channelJoinV1(authUserId, channelId) {
+    return 'authUserId' + 'channelId';
+}

4cf3ffd8725bf71d88a3564cb33281e6f9e7fc65
Theo Ang <z5381420@ad.unsw.edu.au>
2022-06-08 21:34:09 +1000

    added stub channelsListV1 function

---
+++ src/channels.js
@@ -0,0 +1,6 @@
+// Sample stub for a function 'channelsListV1', 
+// with arguments named 'authUserId'
+// Returns a string concatenating 'authUserId'
+function channelsListV1(authUserId) {
+    return 'authUserId';
+}

5287e3027fe0c7d84e3464dc11ddb07237d3d53b
Theo Ang <z5381420@ad.unsw.edu.au>
2022-06-19 18:29:36 +1000

    edited channels.js (not complete)

--- src/channels.js
+++ src/channels.js
@@ -2,25 +2,52 @@
 // with arguments named 'authUserId', 'name', 'isPublic'
 // Returns object with type 'channelId' if no error
 function channelsCreateV1(authUserId, name, isPublic) {
-    return {
-        channelId: 1,
-    };
+
+    new channel = {
+        channelId: generate(channelId),
+        name: name,
+        isPublic: isPublic,
+        ownerMember: [... userProfileV1(authUserId)],
+        allMembers: [... userProfileV1(authUserId)],
+        messages: []
+    }
+    
+    dataStore.channel_list.push (new channel);
+    return {channelId: channel.channelId};
 }
 
 // Sample stub for a function 'channelsListV1', 
 // with arguments named 'authUserId'
 // Returns object with type 'channels' if no error
 function channelsListV1(authUserId) {
-    return {
-        channels: [] // see interface for contents
-    };
+
+    new channels = []
+    
+    for (channel of dataStore.channels) {
+        for (member of channel.allMembers) {
+            if (member.authUserId === authUserId) {
+                channels.push(channel);
+                break;
+            }
+        }
+    }
+
+    return {channels: channels};
 }
 
 // Sample stub for a function 'channelsListallV1', 
 // with arguments named 'authUserId'
 // Returns object with type 'channels' if no error
 function channelsListallV1(authUserId) {
-    return {
-        channels: [] // see interface for contents
-    };
+    
+    new channels = []
+    
+    for (channel of dataStore.channels) {
+        if (member.authUserId === authUserId) {
+            channels.push(channel);
+            break;
+        }
+    }
+
+    return {channels: channels};
 }

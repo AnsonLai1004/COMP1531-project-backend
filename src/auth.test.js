9e9fba2292f839be99092f605b6d15f581255e62
Abigail Bennett Lugue Carungay <a.carungay@student.unsw.edu.au>
2022-06-19 15:51:40 +1000

    wrote authLogin tests

--- src/auth.test.js
+++ src/auth.test.js
@@ -8,34 +8,34 @@ describe('Testing authRegister function error cases', () => {
 
     test('Same email as another user', () => {
         authRegisterV1('same.email@gmail.com', 'password', 'Harry', 'Potter');
-        const registered = authRegisterV1('same.email@gmail.com', 'password', 'Hermione', 'Granger');
+        const registered = authRegisterV1('same@gmail.com', 'password', 'Hermione', 'Granger');
         expect(registered).toStrictEqual({ error: 'error' });
     });
 
     test('Password has five characters', () => {
-        const registered = authRegisterV1('valid.email@gmail.com', 'npass', 'Harry', 'Potter');
+        const registered = authRegisterV1('valid@gmail.com', 'npass', 'Harry', 'Potter');
         expect(registered).toStrictEqual({ error: 'error' });
     });
 
     test('Empty first name', () => {
-        const registered = authRegisterV1('valid.email@gmail.com', 'password', '', 'Potter');
+        const registered = authRegisterV1('valid@gmail.com', 'password', '', 'Potter');
         expect(registered).toStrictEqual({ error: 'error' });
     });
 
     test('Empty last name', () => {
-        const registered = authRegisterV1('valid.email@gmail.com', 'password', 'Harry', '');
+        const registered = authRegisterV1('valid@gmail.com', 'password', 'Harry', '');
         expect(registered).toStrictEqual({ error: 'error' });
     });
     
     test('51 character first name', () => {
         const char = a;
-        const registered = authRegisterV1('valid.email@gmail.com', 'password', char.repeat(51), 'Potter');
+        const registered = authRegisterV1('valid@gmail.com', 'password', char.repeat(51), 'Potter');
         expect(registered).toStrictEqual({ error: 'error' });
     });
 
     test('Empty last name', () => {
         const char = a;
-        const registered = authRegisterV1('valid.email@gmail.com', 'password', 'Harry', char.repeat(51));
+        const registered = authRegisterV1('valid@gmail.com', 'password', 'Harry', char.repeat(51));
         expect(registered).toStrictEqual({ error: 'error' });
     });
 
@@ -43,16 +43,36 @@ describe('Testing authRegister function error cases', () => {
 
 describe('Testing authRegister function valid cases', () => {
     test('Single user', () => {
-        const registered = authRegisterV1('valid.email@gmail.com', 'password', 'Harry', 'Potter');
+        const registered = authRegisterV1('valid@gmail.com', 'password', 'Harry', 'Potter');
         expect(registered).toStrictEqual( 
             expect.objectContaining({
-                authUserId: expect.any(Number),
+                authUserId: expect.any(Number)
           })
         );
     });
 });
 
-describe('Testing authLogin function', () => {
-    // none yet
+describe('Testing authLogin function error cases', () => {
+    test('Email does not belong to a user', () => {
+        const login = authLoginV1('invalid@gmail.com', 'password');
+        expect(login).toStrictEqual({ error: 'error' });
+    });
+
+    test('Wrong password', () => {
+        authRegisterV1('valid@gmail.com', 'password', 'Harry', 'Potter');
+        const login = authLoginV1('valid@gmail.com', 'wrongpassword');
+        expect(login).toStrictEqual({ error: 'error' });
+    });
 });
 
+describe('Testing authLogin function valid cases', () => {
+    test('Valid login', () => {
+        const registered = authRegisterV1('valid@gmail.com', 'password', 'Harry', 'Potter');
+        const login = authLoginV1('valid@gmail.com', 'password');
+        expect(login).toStrictEqual( 
+            expect.objectContaining({
+                authUserId: registered.authUserId
+          })
+        );
+    });
+});

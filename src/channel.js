dc76eaddc59a90eb6b4b0a8944c3d25d66fad230
Anson Lai <z5361987@ad.unsw.edu.au>
2022-06-19 22:16:39 +1000

    Update channelJoin function & added a helper function

--- src/channel.js
+++ src/channel.js
@@ -21,16 +21,19 @@ function channelMessagesV1(authUserId, channelId, start) {
     };
 }
 
-// Sample stub for a function 'channelDetailsV1', 
+
 // with arguments named 'authUserId', 'channelId'
 // Returns a object with types 'name', 'isPublic', 
 // ownerMembers', 'allMembers' if no error
+// Given a channel with ID channelId that the 
+// authorised user is a member of, provide basic 
+// details about the channel.
 function channelDetailsV1(authUserId, channelId) {
     // check if authUserId is valid
     if (!isValidUserId(authUserId)) {
         return { error: 'error' };
     }
-    //
+    // 
     const data = getData();
     for (let channel of data.channels) {
         if (channel.cId === channelId) {
@@ -58,14 +61,54 @@ function channelDetailsV1(authUserId, channelId) {
 // Sample stub for a function 'channelJoinV1', 
 // with arguments named 'authUserId', 'channelId'
 // Returns empty object if no error
+// Given a channelId of a channel that the authorised 
+// user can join, adds them to that channel.
 function channelJoinV1(authUserId, channelId) {
+    if (!isValidChannelId(channelId) || !isValidUserId(authUserId)) {
+        return { error: 'error' };
+    }
+    // check if channel is private, if yes check if user is global owner
+    const channel = channelDetailsV1(channelId);
+    const data = getData();
+    if (channel.isPublic === false) {        
+        for (let user of data.users) {
+            if (authUserId === user.uId) {
+                if (user.isOwner === false) {
+                    return { error: 'error' };
+                }
+            }
+        }
+    }
+    // need to check if user is already a member
+    for (let id of channel.allMembers) {
+        if (authUserId === id) {
+            return { error: 'error' };
+        }
+    }
+    // 
+    for (let channel of data.channels) {
+        if (channelId === channel.cId) {
+            channel.memberIds.push(authUserId);
+            setData(data);
+        }
+    }
     return {};
 }
 
-// Helper function
+// Helper functions
+// return false if channelId does not refer to a valid channel
+function isValidChannelId(channelId) {
+    const data = getData();
+    for (let channel of data.channels) {
+        if (channelId === channel.cId) {
+            return true;
+        }
+    }
+    return false;
+}
 // return false if authUserId is not valid 
 function isValidUserId(authUserId) {
-    const data = getData;
+    const data = getData();
     for (let user of data.users) {
         if (authUserId === user.uId) {
             return true;

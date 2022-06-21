d23dc0850c2362b1b6ac97c5156ad66553b3f22a
Theo Ang <z5381420@ad.unsw.edu.au>
2022-06-21 14:35:45 +1000

    add export tags

--- src/other.js
+++ src/other.js
@@ -1,4 +1,13 @@
+import { getData } from './dataStore.js'
+
 function clearV1() {
+  const data = {
+    users: [],
+    channels: [],
+    lastAuthUserId: 0,
+    lastChannelId: 0,
+  }
+
   return {};
 }
 

4c26f539b061740d6b4a7d1992c16f89363dd2e8
Abigail Bennett Lugue Carungay <a.carungay@student.unsw.edu.au>
2022-06-21 12:15:55 +1000

    added unique handle generation

--- src/auth.js
+++ src/auth.js
@@ -22,7 +22,7 @@ let lastUserId = 0;
 // 'nameFirst' (string), and 'nameLast' (string)
 // returns object containing type 'authUserId' (integer) if no error
 export function authRegisterV1(email, password, nameFirst, nameLast) {
-    if (!isEmail(email) || checkEmailIsUsed(email)) {
+    if (!isEmail(email) || checkDuplicateUserData(email, 'email')) {
         return errorObject;
     }
     if (password.length < 6) {
@@ -34,8 +34,12 @@ export function authRegisterV1(email, password, nameFirst, nameLast) {
     if (nameLast.length < 1 || nameLast.length > 50) {
         return errorObject;
     }
-    const newId = lastUserId + 1;
-    lastUserId = newId;
+
+    const data = getData();
+    const newId = data.lastAuthUserId + 1;
+    data.lastAuthUserId = newId;
+
+    const handle = generateHandle(nameFirst, nameLast);
 
     const newUser = {
         'uId': newId,
@@ -43,14 +47,14 @@ export function authRegisterV1(email, password, nameFirst, nameLast) {
         'nameLast': nameLast,
         'email': email,
         'password': password,
-        'handleStr': 'placeholderhandle',
+        'handleStr': handle,
         'profilePicUrl': '/path/to/image',
         'isOnline': true,
         'isOwner': true,
     };
     
     
-    const data = getData();
+    
     data.users.push(newUser);
     setData(data);
 
@@ -60,16 +64,42 @@ export function authRegisterV1(email, password, nameFirst, nameLast) {
 }
 
 /**
- * Function which checks if an email is already used by another user.
- * @param {string} email 
+ * Function which checks if a particular piece of data is
+ * already used by another user.
+ * @param {string} toCheck
+ * @param {string} field
  * @returns {boolean}
  */
-function checkEmailIsUsed(email) {
+function checkDuplicateUserData(toCheck, field) {
     const data = getData();
-    for (const user in data.users) {
-        if (email === user.email) {
+    for (const user of data.users) {
+        if (toCheck === user[field]) {
             return true;
         }
     }
     return false;
+}
+
+/**
+ * A function which generates a unique user handle based on
+ * first name and last name concatenated, casted to lowercase,
+ * non-alphanumeric characters removed and truncated at 20 characters.
+ * A number index is appended at the end if the handle already exists.
+ * @param {string} nameFirst 
+ * @param {string} nameLast 
+ * @returns {string}
+ */
+const generateHandle = function(nameFirst, nameLast) {
+    let prelimHandle = (nameFirst + nameLast).toLowerCase();
+    prelimHandle = prelimHandle.replace(/[_\W]/g, '');
+    prelimHandle = prelimHandle.slice(0, 20);
+
+    let finalHandle = prelimHandle;
+    let i = 0;
+    while (checkDuplicateUserData(finalHandle, 'handleStr')) {
+        finalHandle = prelimHandle + `${i}`;
+        i++;
+    }
+
+    return finalHandle;
 }

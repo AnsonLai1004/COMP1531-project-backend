315e41dbce907d3f48df7222fbf37c0cd3e455f3
Anson Lai <z5361987@ad.unsw.edu.au>
2022-06-22 15:24:08 +1000

    Update clearV1 function & test

--- src/other.test.js
+++ src/other.test.js
@@ -4,18 +4,36 @@ import { channelsCreateV1 } from './channels.js';
 import { getData } from './dataStore.js';
 
 test('clearV1 test', () => {
-    const userA = authRegisterV1('validemail@gmail.com', '123abc!@#', 'Jake', 'Renzella');
-    const userB = authRegisterV1('Bob@gmail.com', '123abc!@#', 'Bob', 'Renzella');
-    const channelA = channelsCreateV1(userA.authUserId, 'secret candy crush team', true);
-    const channelB = channelsCreateV1(userB.authUserId, 'another channel', true);
+    const userA = authRegisterV1('hayhay123@gmail.com', '8743b52063cd84097a65d1633f5c74f5', 'Hayden', 'Smith');
+    //const userB = authRegisterV1('Bob@gmail.com', '123abc!@#', 'Bob', 'Renzella');
+    //const channelA = channelsCreateV1(userA.authUserId, 'secret candy crush team', true);
+    //const channelB = channelsCreateV1(userB.authUserId, 'another channel', true);
     const data = getData();
-    console.log(data);
-    /*expect(data).toMatchObject({
+    expect(data).toMatchObject({
         users:[
-
+            {
+                'uId': 1,
+                'nameFirst': 'Hayden',
+                'nameLast': 'Smith',
+                'email': 'hayhay123@gmail.com',
+                'password': '8743b52063cd84097a65d1633f5c74f5',
+                'handleStr': 'haydensmith',
+                'profilePicUrl': '/path/to/image',
+                'isOnline': true,
+                'isOwner': true,
+            }
         ],
         channels:[],
-    });*/
-    result = echo('abc');
-    expect(result).toBe('abc');
+        lastAuthUserId: data.users.length,
+        lastChannelId: 0,
+    });
+    clearV1();
+    const dataA = getData();
+    expect(dataA).toMatchObject({
+        users:[],
+        channels:[],
+        lastAuthUserId: 0,
+        lastChannelId: 0,
+    })
+    
   });

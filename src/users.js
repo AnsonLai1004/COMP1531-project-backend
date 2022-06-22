2fa0fc8db87eb6883cd9910e3f09dc2a93023fa4
Abigail Bennett Lugue Carungay <a.carungay@student.unsw.edu.au>
2022-06-22 21:56:47 +1000

    add id checker helper function

--- src/users.js
+++ src/users.js
@@ -1,3 +1,18 @@
+import { getData } from './dataStore.js';
+
+/**
+ * Function which, given a valid authUserId and uId, returns the 
+ * details of the user whose uId matches the argument uId
+ * @param {number} authUserId 
+ * @param {number} uId 
+ * @returns {user: {
+ *  uId: number,
+ *  email: string,
+  * nameFirst: string,
+  * nameFirst: string,
+  * handleStr: string
+ * }}
+ */
 function userProfileV1(authUserId, uId) {
   return {
     user: {
@@ -8,6 +23,21 @@ function userProfileV1(authUserId, uId) {
       handleStr: 'hermionegranger'
     }
   }
+}
+
+/**
+ * Checks if a userId is valid.
+ * @param {number} toCheck 
+ * @returns {boolean}
+ */
+function checkUserIdValid(toCheck) {
+  const data = getData();
+  for (const user of data.users) {
+    if (toCheck === user.uId) {
+        return true;
+    }
+  }
+  return false;
 }
 
 export { userProfileV1 }

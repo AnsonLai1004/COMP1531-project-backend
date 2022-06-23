82357d22e415cc5bdddfbd721666e25f663dc3ed
Anson Lai <z5361987@ad.unsw.edu.au>
2022-06-23 16:46:28 +1000

    other.test.js complete version

--- src/other.test.js
+++ src/other.test.js
@@ -5,27 +5,34 @@ import { getData } from './dataStore.js';
 
 test('clearV1 test', () => {
     const userA = authRegisterV1('hayhay123@gmail.com', '8743b52063cd84097a65d1633f5c74f5', 'Hayden', 'Smith');
-    //const userB = authRegisterV1('Bob@gmail.com', '123abc!@#', 'Bob', 'Renzella');
-    //const channelA = channelsCreateV1(userA.authUserId, 'secret candy crush team', true);
-    //const channelB = channelsCreateV1(userB.authUserId, 'another channel', true);
+    const channelA = channelsCreateV1(userA.authUserId, 'BOOST', true);   
     const data = getData();
     expect(data).toMatchObject({
         users:[
             {
-                'uId': 1,
-                'nameFirst': 'Hayden',
-                'nameLast': 'Smith',
-                'email': 'hayhay123@gmail.com',
-                'password': '8743b52063cd84097a65d1633f5c74f5',
-                'handleStr': 'haydensmith',
-                'profilePicUrl': '/path/to/image',
-                'isOnline': true,
-                'isOwner': true,
+                uId: 1,
+                nameFirst: 'Hayden',
+                nameLast: 'Smith',
+                email: 'hayhay123@gmail.com',
+                password: '8743b52063cd84097a65d1633f5c74f5',
+                handleStr: 'haydensmith',
+                profilePicUrl: '/path/to/image',
+                isOnline: true,
+                isOwner: true,
             }
         ],
-        channels:[],
+        channels:[
+            {
+                name: "BOOST",
+                isPublic: true,
+                channelId: 1,
+                ownerMembers: [ userA.authUserId ],
+                allMembers: [ userA.authUserId ],
+                messages: [],
+            }
+        ],
         lastAuthUserId: data.users.length,
-        lastChannelId: 0,
+        lastChannelId: data.channels.length,
     });
     clearV1();
     const dataA = getData();

5ea29a2babc94936fe5120b17ba115072c4f8b26
Abigail Bennett Lugue Carungay <a.carungay@student.unsw.edu.au>
2022-06-30 16:48:43 +1000

    added object interfaces to dataStore for users, messages, token-pairs, dms. added changes discussed in meeting for dm and message global id generators

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,10 +1,63 @@
 // YOU SHOULD MODIFY THIS OBJECT BELOW
 let data = {
-  users: [],
-  channels: [],
+  users: [] as User[],
+  channels: [] as Channel[],
+  dms: [] as DM[],
+  tokens: [] as TokenPair[],
   lastAuthUserId: 0,
   lastChannelId: 0,
+  lastDMId: 0,
+  lastMessageId: 0
 };
+
+interface User {
+  uId: number;
+  nameFirst: string;
+  nameLast: string;
+  email: string;
+  password: string;
+  handleStr: string;
+  isGlobalOwner: boolean;
+}
+
+interface Message {
+  messageId: number;
+  uId: number;
+  message: string;
+  timeSent: number;
+}
+
+interface Channel {
+  channelId: number;
+  name: string;
+  ownerMembers: number[];
+  allMembers: number[];
+  messages: Message[];
+}
+
+interface DM {
+  dmId: number;
+  name: string;
+  ownerId:  number;
+  uIds: number[];
+  messages: Message[];
+}
+
+interface TokenPair {
+  token: string;
+  uId: number;
+}
+
+interface DataStore {
+  users: User[];
+  channels: Channel[];
+  dms: DM[];
+  tokens: TokenPair[];
+  lastAuthUserId: number;
+  lastChannelId: number;
+  lastDMId: number;
+  lastMessageId: number;
+}
 
 // YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
 
@@ -23,12 +76,12 @@ Example usage
 */
 
 // Use get() to access the data
-function getData() {
+function getData(): DataStore {
   return data;
 }
 
 // Use set(newData) to pass in the entire data object, with modifications made
-function setData(newData) {
+function setData(newData: DataStore) {
   data = newData;
 }
 

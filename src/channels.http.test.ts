95160bc6d519b7d576812470cb80a1fdb04501ff
Theo Ang <z5381420@ad.unsw.edu.au>
2022-07-10 09:27:33 +1000

    simplified tests to only base off channelsCreate (no channelJoin/Details)

--- src/channels.http.test.ts
+++ src/channels.http.test.ts
@@ -49,74 +49,12 @@ describe('Correct Input', () => {
     user5 = requestAuthRegister('dory@gmail.com', 'samplePass', 'Dory', 'Dean');
 
     channel1 = requestChannelsCreate(user1.token, 'BOOST', true).channelId;
-    console.log(channelJoinV1(user2.authUserId, channel1));
-    channelJoinV1(user3.authUserId, channel1);
-    channelJoinV1(user4.authUserId, channel1);
 
     channel2 = requestChannelsCreate(user2.token, 'CRUNCHIE', true).channelId;
-    channelJoinV1(user3.authUserId, channel2);
-    channelJoinV1(user4.authUserId, channel2);
 
     channel3 = requestChannelsCreate(user1.token, 'EGGS', true).channelId;
-    channelJoinV1(user2.authUserId, channel3);
 
     channel4 = requestChannelsCreate(user3.token, 'AERO', false).channelId;
-  });
-
-  test('channelsCreateV1 correct output', () => {
-    const user1Profile = userProfileV1(user1.authUserId, user1.authUserId).user;
-    const user2Profile = userProfileV1(user2.authUserId, user2.authUserId).user;
-    const user3Profile = userProfileV1(user3.authUserId, user3.authUserId).user;
-    const user4Profile = userProfileV1(user4.authUserId, user4.authUserId).user;
-    
-    const channel1Details = channelDetailsV1(user1.authUserId, channel1);
-    const channel2Details = channelDetailsV1(user2.authUserId, channel2);
-    const channel3Details = channelDetailsV1(user1.authUserId, channel3);
-    const channel4Details = channelDetailsV1(user3.authUserId, channel4);
-
-    expect(channel1Details).toStrictEqual({
-      name: 'BOOST',
-      isPublic: true,
-      ownerMembers: [user1Profile],
-      allMembers: expect.any(Array),
-    });
-
-    expect(new Set(channel1Details.allMembers)).toStrictEqual(
-      new Set([user1Profile, user2Profile, user3Profile, user4Profile])
-    );
-
-    expect(channel2Details).toStrictEqual({
-      name: 'CRUNCHIE',
-      isPublic: true,
-      ownerMembers: [user2Profile],
-      allMembers: expect.any(Array),
-    });
-
-    expect(new Set(channel2Details.allMembers)).toStrictEqual(
-      new Set([user2Profile, user3Profile, user4Profile])
-    );
-
-    expect(channel3Details).toStrictEqual({
-      name: 'EGGS',
-      isPublic: true,
-      ownerMembers: [user1Profile],
-      allMembers: expect.any(Array),
-    });
-
-    expect(new Set(channel3Details.allMembers)).toStrictEqual(
-      new Set([user1Profile, user2Profile])
-    );
-
-    expect(channel4Details).toStrictEqual({
-      name: 'AERO',
-      isPublic: false,
-      ownerMembers: [user3Profile],
-      allMembers: expect.any(Array),
-    });
-
-    expect(new Set(channel4Details.allMembers)).toStrictEqual(
-      new Set([user3Profile])
-    );
   });
 
   test('channelsListV1 users 1-5', () => {
@@ -138,16 +76,8 @@ describe('Correct Input', () => {
       channels:
                 [
                   {
-                    channelId: channel1,
-                    name: 'BOOST',
-                  },
-                  {
                     channelId: channel2,
                     name: 'CRUNCHIE',
-                  },
-                  {
-                    channelId: channel3,
-                    name: 'EGGS',
                   },
                 ]
     });
@@ -155,14 +85,6 @@ describe('Correct Input', () => {
     expect(requestChannelsList(user3.token)).toStrictEqual({
       channels:
                 [
-                  {
-                    channelId: channel1,
-                    name: 'BOOST',
-                  },
-                  {
-                    channelId: channel2,
-                    name: 'CRUNCHIE',
-                  },
                   {
                     channelId: channel4,
                     name: 'AERO',
@@ -173,14 +95,6 @@ describe('Correct Input', () => {
     expect(requestChannelsList(user4.token)).toStrictEqual({
       channels:
                 [
-                  {
-                    channelId: channel1,
-                    name: 'BOOST',
-                  },
-                  {
-                    channelId: channel2,
-                    name: 'CRUNCHIE',
-                  },
                 ]
     });
 

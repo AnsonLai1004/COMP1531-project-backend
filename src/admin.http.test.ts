dc974925093a1f383d21d0394b7a0180c6646962
Anson Lai <z5361987@ad.unsw.edu.au>
2022-08-01 20:41:36 +1000

    test update

--- src/admin.http.test.ts
+++ src/admin.http.test.ts
@@ -1,4 +1,4 @@
-import { reqAdminUserRemove, reqAdminUPChange, requestChannelsCreateV3, reqChannelJoin } from './requests';
+import { reqAdminUserRemove, reqAdminUPChange, requestChannelsCreateV3, reqChannelJoin, reqDmCreate } from './requests';
 beforeEach(() => {
     requestClear();
   });
@@ -12,21 +12,57 @@ describe('admin/user/remove/v1', () => {
         expect(reqAdminUserRemove(globalOwner.token, -999)).toStrictEqual(400);
         expect(reqAdminUserRemove(globalOwner.token, globalOwner.authUserId)).toStrictEqual(400);
     });
-    test('correct return', () => {
+    test('tests - channels, Dms, users/all/v2, treat owner can remove other owners', () => {
+        const globalOwner = requestAuthRegister('same@gmail.com', 'password', 'Harry', 'Potter');
+        const channelOwner = requestAuthRegister('same@gmail.com', 'password', 'Hermione', 'Granger');
+        const user = requestAuthRegister('validemail@gmail.com', '123abc!@#', 'Jake', 'Renzella');
+        const channel = requestChannelsCreateV3(channelOwner.token, 'first', true);
+        const channel2 = requestChannelsCreateV3(channelOwner.token, 'second', true);
+        const dm = reqDmCreate(channelOwner.token, [globalOwner.authUserId, user.authUserId]);
+        const dm2 = reqDmCreate(channelOwner.token, [globalOwner.authUserId, user.authUserId]);
         // removed from all channels/DMS
 
+        // TODO: user/stats/v1 return userStats to get numChannelsJoined, numDmsJoined
+        expect(reqChannelJoin(globalOwner.token, channel.channelId));
+        expect(reqChannelJoin(user.token, channel.channelId)).toStrictEqual({});
+        expect(reqChannelJoin(user.token, channel2.channelId)).toStrictEqual({});
+        expect(reqChannelJoin(globalOwner.token, channel.channelId)).toStrictEqual({});
+        expect(reqAdminUserRemove(globalOwner.token, user.authUserId)).toStrictEqual({});
+        // TODO: user/stats/v1 return userStats to get numChannelsJoined, numDmsJoined0
+        /* users: [{
+            uId,email,nameFirst,nameLast,handleStr,
+        }]*/
         // not be included in user array returned by users/all
-
+        // TODO: request users/all/v2 
+        expect(/*users/all/v2*/).toMatchObject([
+            {
+                uId: globalOwner.authUserId,
+                nameFirst: 'Harry',
+                nameLast: 'Potter',
+                handleStr: 'harrypotter',
+            },
+            {
+                uId: channelOwner.authUserId,
+                nameFirst: 'Hermione',
+                nameLast: 'Granger',
+                handleStr: 'hermionegranger',
+            },
+        ]);
         // treats owners can remove other threats owners(including the original first owner)
-
+        const newGO = requestAuthRegister('same@gmail.com', 'password', 'Jordan', 'Potter');
+        expect(reqAdminUPChange(globalOwner.token, newGO.authUserId, 1)).toStrictEqual({});
+        expect(reqAdminUserRemove(newGO.token, globalOwner.authUserId)).toStrictEqual({});   
+    }); 
+    
+    test('test - messages', () => {
         // contents of the messages they sent - replaced by 'Removed user'
+
+
 
         // profile still be retrievable with user/profile
         // but nameFirst - Removed, nameLast - user
         // user's email and handle should be reusable 
 
-    }); 
-    test('', () => {
     });
 });
 

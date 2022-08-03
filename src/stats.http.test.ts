751a7d96bd0019e374a33efd39ce38a0a166842d
Abigail Bennett Lugue Carungay <a.carungay@student.unsw.edu.au>
2022-08-03 15:18:04 +1000

    more test writing

--- src/stats.http.test.ts
+++ src/stats.http.test.ts
@@ -36,12 +36,38 @@ describe('user/stats/v1', () => {
         const time1b  = Math.floor((new Date()).getTime() / 1000);
         const channel1 = requestChannelsCreateV3(user1.token, 'BOOST', true).channelId;
         const time1c = Math.floor((new Date()).getTime() / 1000);
-        reqMessageSend()
+        reqMessageSend(user1.token, channel1.channelId, 'message');
 
+        const stats1b = reqUserStats(user1.token);
+        expect(stats1b).toEqual({ userStats: {
+            channelsJoined:[{numChannelsJoined: 0, timeStamp: expect.any(Number)}, {numChannelsJoined: 1, timeStamp: expect.any(Number)}],
+            dmsJoined:[{numDmsJoined: 0, timeStamp: expect.any(Number)}],
+            messagesSent:[{numMessagesSent: 0, timeStamp: expect.any(Number)}, {numMessagesSent: 1, timeStamp: expect.any(Number)}],
+            involvementRate: 1
+        }});
+
+        expect(stats1b.channelsJoined[1].timeStamp).toBeGreaterThanOrEqual(time1b);
+        expect(stats1b.channelsJoined[1].timeStamp).toBeLessThanOrEqual(time1b + 1);
+        expect(stats1b.messagesSent[1].timeStamp).toBeGreaterThanOrEqual(time1c);
+        expect(stats1b.messagesSent[1].timeStamp).toBeLessThanOrEqual(time1c + 1);
 
         const user2 = requestAuthRegister('alex@gmail.com', 'samplePass', 'Alex', 'Avery');
         
-      
+        const time2a = Math.floor((new Date()).getTime() / 1000);
+        const dm1 = reqDmCreate(user2.token, [user1.authUserId]);
+
+        const time2b = Math.floor((new Date()).getTime() / 1000);
+        reqSendMessageDm(user2.token, dm1.dmId, "dm 1 message");
+        reqSendMessageDm(user1.token, dm1.dmId, "dm 2 message");
+
+        const stats1c = reqUserStats(user1.token);
+        expect(stats1c).toEqual({ userStats: {
+          channelsJoined:[{numChannelsJoined: 0, timeStamp: expect.any(Number)}, {numChannelsJoined: 1, timeStamp: expect.any(Number)}],
+          dmsJoined:[{numDmsJoined: 0, timeStamp: expect.any(Number)}, {numDmsJoined: 1, timeStamp: expect.any(Number)}],
+          messagesSent:[{numMessagesSent: 0, timeStamp: expect.any(Number)}, {numMessagesSent: 1, timeStamp: expect.any(Number)}, {numDmsJoined: 2, timeStamp: expect.any(Number)}],
+          involvementRate: 1
+        }});
+
     });
 });
   
